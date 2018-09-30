1f996c5ec0d4aec7e0552fdb54f1c9bb9dd5c342
DhruvGala <dhruv.g123@gmail.com>
2018-09-30 19:12:36 -0400

    templating engine pug

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
@@ -13,9 +12,11 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
