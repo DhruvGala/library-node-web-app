02b25dc23679dd0331c7b554422c2cdf886a722d
DhruvGala <dhruv.g123@gmail.com>
2018-10-01 21:01:25 -0400

    update in template

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -15,6 +16,21 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/books')
+  .get((req, res) => {
+    res.render('books',
+      {
+        nav: [{
+          link: '/books', title: 'Books'
+        },
+        {
+          link: '/authors', title: 'Authors'
+        }],
+        title: 'Library'
+      });
+  });
+
+app.use('/', bookRouter);
 app.get('/', (req, res) => {
   res.render(
     'index',
