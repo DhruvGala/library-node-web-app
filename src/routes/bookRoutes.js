7e6904005e27e3d4989b259c9bbcf0bdb464058a
DhruvGala <dhruv.g123@gmail.com>
2018-10-29 20:52:53 -0400

    implmented select one bookd with referencing to objectId from MongoClient

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,34 +1,11 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:booksRoutes');
 
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      title: 'How to Be Alone',
-      author: 'Sara Maitland',
-      rating: 4
-    },
-    {
-      title: 'The Alchemist',
-      author: 'Paulo Coelho',
-      rating: 4
-    },
-    {
-      title: 'Alan Turing: Unlocking the Enigma',
-      author: 'David Boyle',
-      rating: 4
-    },
-    {
-      title: 'A Brief History of Time',
-      author: 'Dr. Stephen Hawking',
-      rating: 5
-    },
-  ];
-
   bookRouter.route('/')
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
@@ -65,14 +42,33 @@ function router(nav) {
   bookRouter.route('/:id')
     .get((req, res) => {
       const { id } = req.params;
-      res.render(
-        'bookView',
-        {
-          nav,
-          title: 'Library',
-          book: books[id]
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected to MongoDB server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('books');
+
+          const book = await col.findOne({ _id: new ObjectID(id) });
+
+          res.render(
+            'bookView',
+            {
+              nav,
+              title: 'Library',
+              book
+            }
+          );
+        } catch (err) {
+          debug(err.stack);
         }
-      );
+      }());
     });
 
   return bookRouter;
